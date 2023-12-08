a2a8e2cb2adfe99387c23a1d8eccda27e59e26e1
ipb21.k.birkavs <ipb21.k.birkavs@pjvt.local>
2023-12-08 13:50:46 +0200

    start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel_test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="banner-container">
+        <div class="banner-header">
+            <h2>We value your privacy</h2>
+            <button id="close">X</button>
+        </div>
+        <div class="banner-body">
+            <p>We use cookies to enchance your browsing experience, serve personalized ads
+                or content, and analyze our traffic, By clicking "Accept All", you consent to
+                our use of cookies. <a href="#">Cookie Policy</a>
+            </p>
+        </div>
+        <div class="banner-footer">
+            <button id="customize">Customize</button>
+            <button id="reject">Reject All</button>
+            <button id="accept">Accept All</button>
+        </div>
+    </div>
+</body>
+</html>
